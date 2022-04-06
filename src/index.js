68d809312ba1f989e752220ddbba45edc95376a0
Emmanuel Gutierrez <emma.g2705@gmail.com>
2022-04-06 17:09:33 -0300

    primer commit, se configuro babel,webpack,eslint,prettier y husky

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
+
+const rootElement = document.getElementById('app');
+
+const root = createRoot(rootElement);
+root.render(<App />);
+
+/* ReactDOM.render(<App />, document.getElementById("app")); */
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader',
+                },
+            },
+            {
+                test: /\.(js|jsx)?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.html?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'html-loader',
+                },
+            },
+            {
+                test: /\.(css|scss)$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                type: 'asset',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css',
+        }),
+    ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3005,
+    },
+};
